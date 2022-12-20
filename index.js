c539e2bde0c44e162a463e9f4fc69a1bd8f02fd6
prastyaneki059 <112496805+prastyaneki059@users.noreply.github.com>
2022-12-21 03:52:30 +0700

    readme

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("http").createServer((_, res) => res.end("Berjalan coy")).listen(8080)
 
-const sessionName = 'yusril'
+const sessionName = 'prastyaneki'
 const donet = 'https://saweria.co/sansekai'
-const owner = ['6287878817169']
+const owner = ['6285236611304']
 const { default: sansekaiConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, getContentType } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
